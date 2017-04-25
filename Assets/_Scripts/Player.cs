a9d1e44110e16493281bee8cf79445b337c6c9e0
marc carlyon <irresistiblejelly@gmail.com>
2017-04-25 16:38:20 +0100

    First commit 34 minutes pt 2

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool> { }
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField]
+    ToggleEvent onToggleShared;
+    [SerializeField]
+    ToggleEvent onToggleLocal;
+    [SerializeField]
+    ToggleEvent onToggleRemote;
+    [SerializeField]
+    float respawnTime = 5f;
+
+    GameObject mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main.gameObject;
+
+        EnablePlayer();
+    }
+
+    void DisablePlayer()
+    {
+        if (isLocalPlayer)
+        {
+            PlayerCanvas.canvas.HideReticule();
+            mainCamera.SetActive(true);
+        }
+
+        onToggleShared.Invoke(false);
+
+        if (isLocalPlayer)
+            onToggleLocal.Invoke(false);
+        else
+            onToggleRemote.Invoke(false);
+    }
+
+    void EnablePlayer()
+    {
+        if (isLocalPlayer)
+        {
+            PlayerCanvas.canvas.Initialize();
+            mainCamera.SetActive(false);
+        }
+
+        onToggleShared.Invoke(true);
+
+        if (isLocalPlayer)
+            onToggleLocal.Invoke(true);
+        else
+            onToggleRemote.Invoke(true);
+    }
+
+    public void Die()
+    {
+
+        if(isLocalPlayer)
+        {
+            PlayerCanvas.canvas.WriteGameStatusText("You Died HAHA!");
+            PlayerCanvas.canvas.PlayDeathAudio();
+        }
+
+        DisablePlayer();
+
+        Invoke("Respawn", respawnTime);
+    }
+
+    void Respawn()
+    {
+        if (isLocalPlayer)
+        {
+            Transform spawn = NetworkManager.singleton.GetStartPosition();
+            transform.position = spawn.position;
+            transform.rotation = spawn.rotation;
+        }
+
+        EnablePlayer();
+    }
+}
---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShooting : NetworkBehaviour
+{
+    [SerializeField]
+    float shotCooldown = .3f;
+    [SerializeField]
+    Transform firePosition;
+    [SerializeField]
+    ShotEffectsManager shotEffects;
+
+    [SyncVar (hook = "OnScoreChanged")] int score;
+   
+
+    float elapsedTime;
+    bool canShoot;
+
+    void Start()
+    {
+        shotEffects.Initialize();
+
+        if (isLocalPlayer)
+            canShoot = true;
+    }
+
+    [ServerCallback]
+    void OnEnable()
+    {
+        score = 0;
+    }
+
+    void Update()
+    {
+        if (!canShoot)
+            return;
+
+        elapsedTime += Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1") && elapsedTime > shotCooldown)
+        {
+            elapsedTime = 0f;
+            CmdFireShot(firePosition.position, firePosition.forward);
+        }
+    }
+
+    [Command]
+    void CmdFireShot(Vector3 origin, Vector3 direction)
+    {
+        RaycastHit hit;
+
+        Ray ray = new Ray(origin, direction);
+        Debug.DrawRay(ray.origin, ray.direction * 3f, Color.red, 1f);
+
+        bool result = Physics.Raycast(ray, out hit, 50f);
+
+        if (result)
+        {
+            PlayerHealth enemy = hit.transform.GetComponent<PlayerHealth>();
+
+            if (enemy != null)
+            {
+                bool wasKillShot = enemy.TakeDamage();
+                if (wasKillShot)
+                score++;
+            }
+        }
+
+        RpcProcessShotEffects(result, hit.point);
+    }
+
+    [ClientRpc]
+    void RpcProcessShotEffects(bool playImpact, Vector3 point)
+    {
+        shotEffects.PlayShotEffects();
+
+        if (playImpact)
+            shotEffects.PlayImpactEffect(point);
+    }
+
+    void OnScoreChanged(int value)
+    {
+        score = value;
+        if(isLocalPlayer)
+            PlayerCanvas.canvas.SetKills(value);
+    }
+}
