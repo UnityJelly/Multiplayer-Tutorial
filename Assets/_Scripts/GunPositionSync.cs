d8a88851140f0d962a871ca4a71d14bb4ad631fc
marc carlyon <irresistiblejelly@gmail.com>
2017-04-25 21:09:10 +0100

    Gun Sync started.
    Network still working. Camera look up and down working not synced yet,

---
+++ Assets/_Scripts/GunPositionSync.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class GunPositionSync : NetworkBehaviour
+{
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] Transform handMount;
+    [SerializeField] Transform gunPivot;
+    [SerializeField] float threshhold = 10f;
+    [SerializeField] float smoothing = 5f;
+
+    [SyncVar] float pitch;
+    Vector3 lastOffset;
+    float lastSyncedPitch;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+            gunPivot.parent = cameraTransform;
+    }
+}
