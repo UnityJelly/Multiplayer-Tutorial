a6b3a3d0164d3b781863bf826c1cc5994a16ca5f
marc carlyon <irresistiblejelly@gmail.com>
2017-04-25 22:39:19 +0100

    1:15 minutes all working.
    IK to be added.

--- Assets/_Scripts/GunPositionSync.cs
+++ Assets/_Scripts/GunPositionSync.cs
@@ -6,7 +6,7 @@ public class GunPositionSync : NetworkBehaviour
     [SerializeField] Transform cameraTransform;
     [SerializeField] Transform handMount;
     [SerializeField] Transform gunPivot;
-    [SerializeField] float threshhold = 10f;
+    [SerializeField] float threshold = 10f;
     [SerializeField] float smoothing = 5f;
 
     [SyncVar] float pitch;
@@ -17,5 +17,38 @@ public class GunPositionSync : NetworkBehaviour
     {
         if (isLocalPlayer)
             gunPivot.parent = cameraTransform;
+        else
+            lastOffset = handMount.position - transform.position;
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            pitch = cameraTransform.localRotation.eulerAngles.x;
+            if (Mathf.Abs(lastSyncedPitch - pitch) >= threshold)
+            {
+                print("yay");
+                CmdUpdatePitch(pitch);
+                lastSyncedPitch = pitch;
+            }
+        }
+        else
+        {
+            Quaternion newRotation = Quaternion.Euler(pitch, 0f, 0f);
+
+            Vector3 currentOffset = handMount.position - transform.position;
+            gunPivot.localPosition += currentOffset - lastOffset;
+            lastOffset = currentOffset;
+
+            gunPivot.localRotation = Quaternion.Lerp(gunPivot.localRotation, newRotation, Time.deltaTime * smoothing);
+        }
+    }
+
+    [Command]
+    void CmdUpdatePitch(float newPitch)
+    {
+        print("Yay some more");
+        pitch = newPitch;
     }
 }
