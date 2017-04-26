8961c4bb5b46459b495d8fe9cbda8c03f09362d6
marc carlyon <irresistiblejelly@gmail.com>
2017-04-26 07:49:21 +0100

    Fix to Health not starting at 3 on server
    Added void Start to set server health to 3 if also local player.

--- Assets/_Scripts/PlayerHealth.cs
+++ Assets/_Scripts/PlayerHealth.cs
@@ -21,6 +21,12 @@ public class PlayerHealth : NetworkBehaviour
         health = maxHealth;
     }
 
+    [ServerCallback]
+    void Start()
+    {
+        health = maxHealth;
+    }
+
     [Server]
     public bool TakeDamage()
     {
